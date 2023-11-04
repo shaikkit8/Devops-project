157e06a0135e90177249f47f44482a7802f85520
shaikkit8 <shaikkit8@gmail.com>
2023-11-04 21:29:15 +0300

    Roboshop Frontend Script

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,6 @@ cp ${script_location}/file/mongodb.repo/ /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-org -y
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongodb
 systemctl restart  mongodb
