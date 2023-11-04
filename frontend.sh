1bd137bfc657e8d6c55fb3e671bcd9c77d45617b
shaikkit8 <shaikkit8@gmail.com>
2023-11-04 21:10:13 +0300

    Frontend

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,4 @@
+script_location=$(pwd)
 dnf install nginx -y
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
@@ -5,7 +6,7 @@ cd /usr/share/nginx/html
 
 unzip /tmp/frontend.zip
 
-cp /file/roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${script_location}/file/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl enable nginx
 systemctl start nginx
